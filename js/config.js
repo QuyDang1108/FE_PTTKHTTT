93298f68a5da17f0017b3d1d79c6d80050e310fd
TaiVo030104 <22120319@student.hcmus.edu.vn>
2025-06-28 13:14:26 +0700

    feature/add-cadidate-ui-and-login

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// Cấu hình thông tin đăng nhập
+const CONFIG = {
+    EMAIL: 'nvtn@gmail.com',
+    PASSWORD: 'nvtn12345'
+};
+
+// Export để sử dụng trong các file khác
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = CONFIG;
+} 
